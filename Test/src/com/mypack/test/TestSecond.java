b8745887ca051c70e1b9a126b9fe4d1f0ac0fe29
manoj kumar <manojmanoj336@gmail.com>
2019-12-11 22:49:16 +0530

    change is done in uiconfigg for pushing in branch Master

--- Test/src/com/mypack/test/TestSecond.java
+++ Test/src/com/mypack/test/TestSecond.java
@@ -2,7 +2,7 @@ package com.mypack.test;
 
 public class TestSecond {
 	public static void main(String[] args) {
-		System.out.println("this change is done for pushing in branch ebill-ui-config");
+		System.out.println("this change is done for pushing in branch Master");
 	}
 	
 }
