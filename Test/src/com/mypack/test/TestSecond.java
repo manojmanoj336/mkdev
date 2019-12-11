5b8b3fb751f4132acf5419680f390cf1c25e967e
manoj kumar <manojmanoj336@gmail.com>
2019-12-11 22:44:15 +0530

    change to commit in ebil ui config

--- Test/src/com/mypack/test/TestSecond.java
+++ Test/src/com/mypack/test/TestSecond.java
@@ -1,5 +1,8 @@
 package com.mypack.test;
 
 public class TestSecond {
-
+	public static void main(String[] args) {
+		System.out.println("this change is done for pushing in branch ebill-ui-config");
+	}
+	
 }
