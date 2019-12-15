bb1596fefc6fa5aa610d2ca638a83fbfeea1bf9c
manoj kumar <manojmanoj336@gmail.com>
2019-12-16 01:01:54 +0530

    after conflict add this line. and commit to local git.

--- Test/src/com/mypack/test/HelloWorld.java
+++ Test/src/com/mypack/test/HelloWorld.java
@@ -1,6 +1,10 @@
 package com.mypack.test;
 
 public class HelloWorld {
+	public void getData()
+	{
+		System.out.println("after conflict add this line. and commit to local git.");
+	}
 	
 	public static void main(String[] args) {
 		System.out.println("hi this is now a to bt");
